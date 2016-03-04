7d99a5af383de940f24b408525d69c162f36c0b3
xuke918 <xuke3070@gmail.com>
2016-03-04 11:38:49 -0500

    Added a : to fit the video.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
-var letter = prompt('Enter a letter');
+var letter = prompt('Enter a letter:');
 
 var i;
 var j;
 
 for (i = 0; i < 10; i++) {
+
   for (j = 0; j < i; j++) {
     document.write(letter);
   }
