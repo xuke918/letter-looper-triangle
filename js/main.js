4c4fd966e6535679ea6ae53f889e99ad4f978a91
xuke918 <xuke3070@gmail.com>
2016-03-04 11:26:41 -0500

    Changed counter to i.

--- js/main.js
+++ js/main.js
@@ -3,10 +3,9 @@ var j;
 
 var letter = prompt('Enter a letter');
 
-var counter;
-
-for (counter = 0; counter < 10; counter++) {
-  for (j = 0; j < i; j++)
-  (document.write(letter);
+for (i = 0; i < 10; i++) {
+  for (j = 0; j < i; j++) {
+    (document.write(letter);
+  }
   document.write('<br>');
 }
