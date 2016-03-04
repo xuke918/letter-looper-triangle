ddb7dd68b9666a2f91a3e9c2831d8f813f03134e
xuke918 <xuke3070@gmail.com>
2016-03-04 11:30:16 -0500

    Added var j.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
+var letter = prompt('Enter a letter');
+
 var i;
 var j;
 
-var letter = prompt('Enter a letter');
-
 for (i = 0; i < 10; i++) {
   for (j = 0; j < i; j++) {
-    (document.write(letter);
+    document.write(letter);
   }
+
   document.write('<br>');
 }
